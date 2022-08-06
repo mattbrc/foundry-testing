6659a9e5cf587756be81da110d4b342249f46f72
Matt <matt.b.wilder1@gmail.com>
2022-08-06 09:44:08 -0600

    update test

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import "forge-std/Test.sol";
+import "ds-test/test.sol";
 import "../src/StakeContract.sol";
 import "./mocks/MockERC20.sol";
 
-contract StakeContractTest is Test {
+contract StakeContractTest is DSTest {
     StakeContract public stakeContract;
     MockERC20 public mockToken;
 
